7c2191fccc8f080229f225fe6bd5899f3f864904
chhotuthakur <npsdahiya@gmail.com>
2023-04-05 17:38:48 +0530

    first commit

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyD6KQiQ1e89UGXRlfXNsEgeG5IwXVG4Es0",
+    authDomain: "hope-69069.firebaseapp.com",
+    databaseURL: "https://hope-69069-default-rtdb.firebaseio.com",
+    projectId: "hope-69069",
+    storageBucket: "hope-69069.appspot.com",
+    messagingSenderId: "793033108097",
+    appId: "1:793033108097:web:3d2f27241caabdd34e3b80",
+    measurementId: "G-56Y5PLMZSS"
+  };
+  firebase.initializeApp(config);
