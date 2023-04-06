55cc2d51c37e4a265412c7089a1295a1e69abfb0
chhotuthakur <npsdahiya@gmail.com>
2023-04-06 09:44:00 +0530

    added authentication

---
+++ assets/signin.js
@@ -0,0 +1,22 @@
+var config = {
+
+    apiKey: "AIzaSyBlKkd3wXSQ6ONwgDl7lzAI6fb2mbMo2RQ",
+    authDomain: "clients-97258.firebaseapp.com",
+    databaseURL: "https://clients-97258-default-rtdb.firebaseio.com",
+    projectId: "clients-97258",
+    storageBucket: "clients-97258.appspot.com",
+    messagingSenderId: "396360667640",
+    appId: "1:396360667640:web:d1f91a54d99d786287460d",
+    measurementId: "G-LP0VZ1N88K"
+
+
+
+    // apiKey: "AIzaSyD6KQiQ1e89UGXRlfXNsEgeG5IwXVG4Es0",
+    // authDomain: "hope-69069.firebaseapp.com",
+    // databaseURL: "https://hope-69069-default-rtdb.firebaseio.com",
+    // projectId: "hope-69069",
+    // storageBucket: "hope-69069.appspot.com",
+    // messagingSenderId: "793033108097",
+    // appId: "1:793033108097:web:3d2f27241caabdd34e3b80",
+    // measurementId: "G-56Y5PLMZSS"
+  };
