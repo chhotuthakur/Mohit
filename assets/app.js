072130764c8d921b6e02f5eefd47017a34ba8d52
Nilesh Singh Dahiya <67730060+chhotuthakur@users.noreply.github.com>
2023-05-17 15:37:00 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,6 @@
 var config = {
 
-
-    
-    
-
-  apiKey: "AIzaSyAWq5dU6zRXXo_zPNFnd7mPOZg3mNooAs0",
+ apiKey: "AIzaSyAWq5dU6zRXXo_zPNFnd7mPOZg3mNooAs0",
   authDomain: "match-making-234ad.firebaseapp.com",
   databaseURL: "https://match-making-234ad.firebaseio.com",
   projectId: "match-making-234ad",
@@ -12,13 +8,6 @@ var config = {
   messagingSenderId: "540215091505",
   appId: "1:540215091505:web:e918e1ca7cd3f5c9c5d010",
   measurementId: "G-T82ZYN4KBY"
-
-
-
-
-
-
-
 
 
     // apiKey: "AIzaSyBlKkd3wXSQ6ONwgDl7lzAI6fb2mbMo2RQ",
@@ -31,12 +20,6 @@ var config = {
     // measurementId: "G-LP0VZ1N88K"
 
     
-    
-    
-    
-    
-    
-    
 //     apiKey: "AIzaSyBlKkd3wXSQ6ONwgDl7lzAI6fb2mbMo2RQ",
 //     authDomain: "clients-97258.firebaseapp.com",
 //     databaseURL: "https://clients-97258-default-rtdb.firebaseio.com",
@@ -45,8 +28,6 @@ var config = {
 //     messagingSenderId: "396360667640",
 //     appId: "1:396360667640:web:d1f91a54d99d786287460d",
 //     measurementId: "G-LP0VZ1N88K"
-
-
 
 
     // apiKey: "AIzaSyD6KQiQ1e89UGXRlfXNsEgeG5IwXVG4Es0",
